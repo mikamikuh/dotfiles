b46b5ec1a332103fafae68d4e52d9ac24466c10e
Kohei Mikami <kohei@zavamed.com>
2021-06-07 18:02:44 +0100

    Add Brewfile and  nvm/pyenv config

--- zsh/mikamikuh.zsh
+++ zsh/mikamikuh.zsh
@@ -26,3 +26,15 @@ bindkey '^R' peco-history-selection
 bindkey -s '^T' 'ranger-cd\n'
 
 alias rc='ranger-cd'
+setopt share_history
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+
+nvm use 14.17.0
+pyenv global 3.9.4
+
+if command -v pyenv 1>/dev/null 2>&1; then
+  eval "$(pyenv init -)"
+fi
